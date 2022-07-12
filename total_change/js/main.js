2c32fc60c0e679f1e2c03b71df0a4dde290aaa8d
Aleksandr Ivanov <74289060+Aleksandr644@users.noreply.github.com>
2022-07-12 15:31:28 +0500

    create a new code

---
+++ total_change/js/main.js
@@ -0,0 +1,19 @@
+const countElement = (text, elem) => {
+	console.log("подсчет");
+	return text.split(elem).length -1;
+}
+
+const output_text = (name, text) => {
+	let text_form = document.getElementById("output_form");
+	text_form.innerHTML = text_form.innerHTML + name + ": " + text + "<br>";
+	while(countElement(text_form.innerHTML, "<br>") > 32){
+		text_form.innerHTML = text_form.innerHTML.slice(text_form.innerHTML.indexOf("<br>") + 4);
+	}
+	console.log(countElement(text_form.innerHTML));
+}
+document.querySelector("#input_button").onclick = function(){
+	let input_text = document.querySelector("#input_form");
+	output_text(document.querySelector("#name_player").innerHTML, input_text.value);
+	input_text.value = "";
+	console.log("вставка текста");
+}
